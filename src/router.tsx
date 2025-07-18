b5e922b42534372190d0b0ea0ec4a221457134e1
developerdesigner18 <developer.designer18@gmail.com>
2025-07-18 14:45:53 +0530

    feat:add proper routing to project

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Categories from './pages/Categories';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+
+  {
+    path: '/categories',
+    element: <Categories />,
+  },
+]);
