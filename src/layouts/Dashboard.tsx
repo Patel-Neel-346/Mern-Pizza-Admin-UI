fcf2c76286f61b7e03a12bb00c08f26d4aed6d50
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-07-31 21:30:57 +0530

    add:added Advanced Techniques for Creating Protected Routes

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuthStore } from "../store/store"
 
 
 const Dashboard = () => {
+
+    const { user } = useAuthStore()
+
+    if (user === null) {
+        return <Navigate to="/auth/login" replace={true} />
+    }
     return (
         <div>
             DashBoard Component
--- src/layouts/NonAuth.tsx
+++ src/layouts/NonAuth.tsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuthStore } from "../store/store"
 
 const NonAuth = () => {
+
+    const { user } = useAuthStore()
+
+    if (user !== null) {
+        return <Navigate to="/" replace={true} />
+    }
     return (
         <div>
             NonAuthComponent
